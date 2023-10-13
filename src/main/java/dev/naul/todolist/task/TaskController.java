1fb094ac15dc1955fe51be1e57b9a75c5363e755
Naul-Epilef <lfps1999@gmail.com>
2023-10-12 22:54:48 -0300

    List tasks by user

--- src/main/java/dev/naul/todolist/task/TaskController.java
+++ src/main/java/dev/naul/todolist/task/TaskController.java
@@ -6,8 +6,8 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,10 +32,19 @@ public class TaskController {
         }
 
         if (taskModel.getStartTime().isAfter(taskModel.getFinishTime())) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início deve vir depois da data de fim.");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início deve vir depois da data de fim'.");
         }
 
         var task = this.taskRepository.save(taskModel);
         return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+
+    @GetMapping
+    public ResponseEntity listByUser(HttpServletRequest request) {
+        var userId = request.getAttribute("userId");
+
+        var tasks = this.taskRepository.findByUserId((UUID) userId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(tasks);
     }
 }
