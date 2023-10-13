b5bd55efdbf922b2487c3dd503a582bd638dc659
Naul-Epilef <lfps1999@gmail.com>
2023-10-12 22:12:24 -0300

    Adding dynamic userId to task

--- src/main/java/dev/naul/todolist/task/TaskController.java
+++ src/main/java/dev/naul/todolist/task/TaskController.java
@@ -1,12 +1,17 @@
 package dev.naul.todolist.task;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/tasks")
@@ -15,7 +20,9 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping
-    public ResponseEntity create(@RequestBody TaskModel taskModel) {
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+        var userId = request.getAttribute("userId");
+        taskModel.setUserId((UUID) userId);
         var task = this.taskRepository.save(taskModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(task);
     }
